8e6a75c9312abb0f8ee9e6a51823e16fe060a31b
Blane Legant <blane.legant@gmail.com>
2017-12-14 18:17:14 -0500

    add CHIP8

---
+++ src/com/blanelegant/chip8008/Chip8.java
@@ -0,0 +1,17 @@
+package com.blanelegant.chip8008;
+
+/**
+ * This is the main instruction interpreter for the chip8008 emulator.
+ */
+
+public class Chip8 {
+
+    // set up basic registers
+    // char has length of 16 bits
+    char current_opcode;
+
+    public Chip8() {
+        
+    }
+
+}
